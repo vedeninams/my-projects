551ca15b1f97e7d785c0c7f974ce2d6241b91468
Мария <maria@MacBook-Air-Maria.local>
2017-03-23 13:29:49 +0300

    update

--- ugolek/js/script.js
+++ ugolek/js/script.js
@@ -1,5 +1,4 @@
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
-      // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+$(function () { 
       $("#navbarToggle").blur(function (event) {
               var screenWidth = window.innerWidth;
               if (screenWidth < 768) {
@@ -11,7 +10,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
       // on the clicked button. Therefore, the blur event will not fire on
       // user clicking somewhere else in the page and the blur event handler
       // which is set up above will not be called.
-      // Solution: force focus on the element that the click event fired on
+      // So force focus on the element that the click event fired on
       // In Firefox and Safari
       $("#navbarToggle").click(function (event) {
               $(event.target).focus();
@@ -39,13 +38,12 @@ document.addEventListener("DOMContentLoaded",
 
     showLoading("#content-snippet");
 
-    // myGetRequest = function () {
     $ajaxUtils.sendGetRequest("snippets/home-snippet.html", 
           function (request) {
             document.querySelector("#content-snippet")
             .innerHTML = request.responseText;
           }); 
-    // var myObj ={};
+
     document.onclick = function(event) {
                   event = event || window.event;
                   if (!event.target) {
@@ -56,13 +54,11 @@ document.addEventListener("DOMContentLoaded",
                   var targetId ="#";
                   var html = "snippets/";
 
-
                   if (event.target.id == "") {
                     targetId += event.target.parentNode.id;
                   } else {
                     targetId += event.target.id;
                     }
-                  
 
                   if (targetId == "#jsmenu") { 
                     html += "menu.html";} else {
@@ -76,8 +72,6 @@ document.addEventListener("DOMContentLoaded",
                           }
                       }
                  
-
-
                   if (targetId == "#jsmenu" || 
                     targetId == "#jshome"|| 
                     targetId == "#jsabout" ||
@@ -86,58 +80,5 @@ document.addEventListener("DOMContentLoaded",
                         function (request) {
                         document.querySelector(selector)
                         .innerHTML = request.responseText;});
-                  }
-    }
-// 
-
-
-}); // addEventListener("DOMContentLoaded",
-
-//////////////////////////
-// document.addEventListener("DOMContentLoaded",
-//   function (event) {
-//     // myGetRequest = function () {
-//     // $ajaxUtils.sendGetRequest(html, 
-//     //       function (request) {
-//     //         document.querySelector(selector)
-//     //         .innerHTML = request.responseText;
-//     //       }); 
-//     // };
-//     // var myObj ={};
-//     document.onclick = function(event) {
-//                   event = event || window.event;
-//                   if (!event.target) {
-//                     event.target = event.srcElement;
-//                   }
-//                   // console.log("MY ID after event.target.id - "+event.target.id); 
-//                   var targetId ="#";
-
-//                   if (event.target.id == "") {
-//                     // console.log("Parent ID - "+event.target.parentNode.id);
-//                     targetId += event.target.parentNode.id;
-//                   } else {
-//                     targetId += event.target.id;
-//                     }
-                  
-    
-//                   // console.log("2.2. after myObj.targetId - "+ targetId); 
-
-//                   var selector = "#content-snippet";
-//                   var html = "snippets/home-snippet.html";
-
-//                   if (targetId == "#jsmenu") { html = "snippets/menu.html";}
-
-//                   if (targetId == "#jsmenu" || 
-//                     targetId == "#jsindex"|| 
-//                     targetId == "#jsmenu" ) {
-//                         $ajaxUtils.sendGetRequest(html, 
-//                         function (request) {
-//                         document.querySelector(selector)
-//                         .innerHTML = request.responseText;});
-//                   }
-//     }
-// // 
-
-
-// }); // addEventListener("DOMContentLoaded",
-
+                  }}
+});
